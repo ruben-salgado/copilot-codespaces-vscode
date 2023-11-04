5f19cc1ae49391e323031fd6aab22059677b741b
Rubén Salgado <60362036+ruben-salgado@users.noreply.github.com>
2023-11-04 05:03:19 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,31 @@
+function skillsMember() {
+    var $skills = $('.skills');
+    if ($skills.length) {
+        var $skill = $('.skill');
+        var $skillPercent = $('.skill-percent');
+        var $skillPercentValue = $('.skill-percent-value');
+        var $skillPercentValueSpan = $('.skill-percent-value span');
+        var $skillPercentValueSpanData = $('.skill-percent-value span').data('percent');
+        var $skillPercentValueSpanDataWidth = $('.skill-percent-value span').data('percent') + '%';
+        var $skillPercentValueSpanWidth = $('.skill-percent-value span').width($skillPercentValueSpanDataWidth);
+        var $skillPercentValueSpanDataWidth = $('.skill-percent-value span').width($skillPercentValueSpanDataWidth);
+        var $skillPercentValueSpanWidthAnimate = $('.skill-percent-value span').animate({
+            width: $skillPercentValueSpanDataWidth
+        }, 1000);
+        var $skillPercentValueSpanDataWidthAnimate = $('.skill-percent-value span').animate({
+            width: $skillPercentValueSpanDataWidth
+        }, 1000);
+        var $skillPercentValueSpanWidthAnimate = $('.skill-percent-value span').animate({
+            width: $skillPercentValueSpanDataWidth
+        }, 1000);
+        var $skillPercentValueSpanDataWidthAnimate = $('.skill-percent-value span').animate({
+            width: $skillPercentValueSpanDataWidth
+        }, 1000);
+        var $skillPercentValueSpanWidthAnimate = $('.skill-percent-value span').animate({
+            width: $skillPercentValueSpanDataWidth
+        }, 1000);
+        var $skillPercentValueSpanDataWidthAnimate = $('.skill-percent-value span').animate({
+            width: $skillPercentValueSpanDataWidth
+        }, 1000);
+    }
+}
